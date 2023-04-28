991ff2047e896c5c855d336f67ae5f7910aa068f
Siyuan He <he.siyua@northeastern.edu>
2023-04-28 13:21:10 -0400

    final v2

--- cp-librabry/WebsiteView.swift
+++ cp-librabry/WebsiteView.swift
@@ -12,20 +12,20 @@ struct WebsiteView: View {
         VStack{
             WebView(urlString: "https://codeforces.com/")
         }
-        .toolbar {
-            ToolbarItem(placement: .principal) {
-                HStack{
-                    Image(systemName: "arrowshape.turn.up.backward.fill")
-                                    .resizable()
-                                    .frame(width: 25, height: 20)
-                                    .foregroundColor(Color(hex: "#7f8c8d"))
-                                    .padding(.leading, -200)
-//                    Text("Problem Details")
-//                        .font(Font.custom("BrunoAceSC-Regular", size: 25))
-//                        .foregroundColor(Color(hex: "#16a085"))
-                }
-            }
-        }
+//        .toolbar {
+//            ToolbarItem(placement: .principal) {
+//                HStack{
+//                    Image(systemName: "arrowshape.turn.up.backward.fill")
+//                                    .resizable()
+//                                    .frame(width: 25, height: 20)
+//                                    .foregroundColor(Color(hex: "#7f8c8d"))
+//                                    .padding(.leading, -200)
+////                    Text("Problem Details")
+////                        .font(Font.custom("BrunoAceSC-Regular", size: 25))
+////                        .foregroundColor(Color(hex: "#16a085"))
+//                }
+//            }
+//        }
     }
 }
 
